04fd07fa327779a4c8b3cde00fc92084ed02149d
Harshit Ranjan <137553967+harshitrnjn@users.noreply.github.com>
2023-08-27 02:31:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,118 @@
+gsap.from(".left i", {
+    opacity: 0,
+    delay: 0,
+    duration: 1,
+    y: 20,
+    ease: Expo.easeInOut
+})
+gsap.from(".left h2", {
+    opacity: 0,
+    delay: 0.5,
+    duration: 1,
+    y: 20,
+    ease: Expo.easeInOut
+})
+gsap.from(".search", {
+    opacity: 0,
+    delay: 0.5,
+    duration: 1,
+    y: 20,
+    ease: Expo.easeInOut
+})
+gsap.from(".img", {
+    opacity: 0,
+    delay: 0.7,
+    duration: 1.5,
+    width: "0px",
+    ease: Expo.easeInOut
+})
+gsap.from(".img img", {
+    opacity: 0,
+    delay: 1.7,
+    duration: 1.5,
+    width: "0px",
+    ease: Expo.easeInOut
+})
+gsap.from(".line-one", {
+    opacity: 0,
+    delay: 2.3,
+    duration: 1.5,
+    y: 30,
+    ease: Expo.easeInOut
+})
+gsap.from(".line-two", {
+    opacity: 0,
+    delay: 2.7,
+    duration: 1.5,
+    y: -50,
+    ease: Expo.easeInOut
+})
+gsap.from(".line-three", {
+    opacity: 0,
+    delay: 3.1,
+    duration: 1.5,
+    y: -70,
+    ease: Expo.easeInOut
+})
+gsap.from(".text2", {
+    opacity: 0,
+    delay: 3.5,
+    duration: 1.5,
+    x: -50,
+    ease: Expo.easeInOut
+})
+gsap.from(".bottom-text", {
+    opacity: 0,
+    delay: 4,
+    duration: 1.5,
+    y: 50,
+    ease: Expo.easeInOut
+})
+gsap.from(".top-text p", {
+    opacity: 0,
+    delay: 4,
+    duration: 1.5,
+    y: -20,
+    ease: Expo.easeInOut
+})
+gsap.from(".line", {
+    opacity: 0,
+    delay: 3.8,
+    duration: 2,
+    width: "0px",
+    ease: Expo.easeInOut
+})
+gsap.from(".btn", {
+    opacity: 0,
+    delay: 4.5,
+    duration: 1.7,
+    y: -20,
+    ease: Expo.easeInOut
+})
+gsap.from(".fb", {
+    opacity: 0,
+    delay: 5,
+    duration: 1,
+    y: -30, ease: Expo.easeInOut
+})
+gsap.from(".insta", {
+    opacity: 0,
+    delay: 5.2,
+    duration: 1,
+    y: -30, ease: Expo.easeInOut
+})
+gsap.from(".snap", {
+    opacity: 0,
+    delay: 5.3,
+    duration: 1,
+    y: -30,
+    ease: Expo.easeInOut
+})
+gsap.from(".arrows", {
+    opacity: 0,
+    delay: 5.4,
+    duration: 1,
+    y: 30,
+    ease: Expo.easeInOut,
+    stagger:1.2
+})
